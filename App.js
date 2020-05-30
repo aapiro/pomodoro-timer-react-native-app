3e091d9e5059033dca42351ca37ffa54a9b69dc2
Anthony  Piñero <anthonypinero1@gmail.com>
2020-05-29 23:28:05 -0500

    creating gitignore

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View,Button,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
 import {vibrate} from './utils'
 import Todo from './Todo.js'
 
